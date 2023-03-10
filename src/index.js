254aae981280f96d9d895b5da35622ed6dc6b5b4
James Dick <jimandjo.geo@yahoo.com>
2023-03-10 08:15:14 -0500

    Cleaning up the commits

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import NavBar from './navbar';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
+    <NavBar/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
