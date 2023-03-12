66d3ed2ebc15f4e8973189b17ce63407a94d5aff
James Dick <jimandjo.geo@yahoo.com>
2023-03-12 11:25:27 -0400

    Starting Over

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
 
-
-
-
 function App() {
   return (
-<>
-<h1 align="Center">Hi</h1>
-
-
-
-
-
-</>
+    <div style={{ textAlign: 'center' }}>
+      <header>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import NavBar from './navbar';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <NavBar/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
