7e8de05cc2a10e8c9f06b3bbc76553e092055c71
James Dick <jimandjo.geo@yahoo.com>
2023-03-12 13:33:38 -0400

    Added the first route.

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,26 @@
 
+import { Link } from "react-router-dom"
+import { Navbar } from "react-bootstrap"
+import { Nav } from "react-bootstrap"
+import Container from "react-bootstrap/Container"
 
 function Home() {
     return (
-
         <>
-        <h1>
-            this is the home page
-        </h1>
+                    {/* Navbar */}
+            <Navbar bg="warning" variant="dark">
+                <Container>
+                    <Nav className="me-auto">
+                        <Link to="/about" className="nav-link">About</Link>
+                    </Nav>
+                </Container>
+                </Navbar>
+
+                <h1>
+                    this is the home page
+                </h1>
+
+               
         </>
     )
 }
