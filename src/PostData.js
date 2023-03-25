83cc4f1a0958d99e75c07ea03794d777ac48f6a0
James Dick <jimandjo.geo@yahoo.com>
2023-03-25 09:23:35 -0400

    Added data file for my posts

---
+++ src/PostData.js
@@ -0,0 +1,10 @@
+const posts = [
+    {
+        title: "Transformation",
+        date: "January 27, 2024",
+        body: "As you read this blog and take action on what we talk about, there is a transformation that will happen for you. You will rest easier, become more relaxed, and find joy in using the internet that is at yourfinger tips. Of course, there are a lot of other things that could be creating anxiety for you, but we hope it is not the fear of being hacked.\n I have experienced that fearful feeling of wondering if my internet was hacked and what it will cost to fix it. See the post Why I Car About Computer Security for the back story. Since taking personal control over my home internet and using the tools I share in this blog, I have not worried about being hacked."
+    
+    }
+]
+
+export default posts
