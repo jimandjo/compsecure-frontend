cd59b417bc5987731ac4e4370708df10b1a34369
James Dick <jimandjo.geo@yahoo.com>
2023-04-02 08:00:12 -0400

    Copy of code completed for data rendering

--- src/Posts.js
+++ src/Posts.js
@@ -1,10 +1,19 @@
+import { Card } from "react-bootstrap"
+import posts from "./PostData"
+
 function Posts() {
+    let info = posts[1]
     return (
-    <>
-    <h1>
-        Posts
-    </h1>
-    </>
+<>
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>{info.title.toUpperCase()}</Card.Title>
+        <Card.Subtitle>{info.date}</Card.Subtitle>
+        <Card.Text>{info.body}</Card.Text>
+      </Card.Body>
+    </Card>
+</>
+    
     )
 }
 export default Posts
