c9d32e6ef7eae50ec4a87e24e368425a0066ee78
James Dick <jimandjo.geo@yahoo.com>
2023-04-24 12:00:27 -0400

    Cleanup before moving to post features

--- src/contexts/PostProvider.js
+++ src/contexts/PostProvider.js
@@ -1,14 +1,21 @@
 import axios from "axios";
-import PostContext from "./PostContext";
 import { useState } from "react";
+import PostContext from "./PostContext";
 
 export const PostProvider = (props) => {
 
     const [ post, setPost ] = useState([]);
-    const baseUrl = "http://localhost:3000/api/coffee/";
+    const baseUrl = "http://localhost:3000/api/posts/";
+
+    useEffect(() => {
+        async function fetchData() {
+            await getAllPost();
+        }
+        fetchData();
+    }, []);
 
     function getAllPost() {
-
+        return axios.get(baseUrl).then(response => setPost(response.data));
     }
 
     function getPost(id) {
